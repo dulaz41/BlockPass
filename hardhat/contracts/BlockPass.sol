0f578470cf40e90accc67a6c792826ae91fd556e
dulaz41 <dulaz41@yahoo.com>
2023-12-04 15:43:47 +0100

    Integrate Chainlink's VRF to ensure fair and transparent distribution of limited edition NFT event tickets

--- hardhat/contracts/BlockPass.sol
+++ hardhat/contracts/BlockPass.sol
@@ -1,16 +1,25 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.20;
 pragma experimental ABIEncoderV2;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract BlockPass is Ownable, ERC721URIStorage {
+import "./PriceConverter.sol";
+
+/**
+ * @title BlockPass
+ * @dev The main smart contract for creating and managing blockpasses.
+ */
+contract BlockPass is Ownable, ERC721URIStorage, VRFV2WrapperConsumerBase {
     using Counters for Counters.Counter;
     Counters.Counter private tokenId;
 
-    // Struct to store details of each block pass
+    // Struct to store details of each blockpass
     struct BlockPassDetails {
         address organizer;
         string metadata;
@@ -24,16 +33,29 @@ contract BlockPass is Ownable, ERC721URIStorage {
         bool bpEnded;
     }
 
+    struct RequestStatus {
+        uint256 paid;
+        bool fulfilled;
+        uint256[] randomWords;
+    }
+
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 2;
     uint256 private blockPass_count = 0;
 
-    // Arrays to store lists of block passes and mappings for quick access
+    // Arrays
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
     BlockPassDetails[] public blockPassList;
+
+    mapping(uint256 => RequestStatus) public requestStatuses;
     mapping(uint256 => BlockPassDetails) public getPassById;
     mapping(address => BlockPassDetails[]) public bookedPassByUser;
     mapping(address => uint256[]) public userNftTokens;
     mapping(address => BlockPassDetails[]) public blockPassCreatedByOrganizer;
 
-    // Events emitted on successful operations
+    // Events
     event blockPassCreated(
         address indexed organizer,
         uint256 indexed creationTime,
@@ -44,17 +66,28 @@ contract BlockPass is Ownable, ERC721URIStorage {
         uint256 tokenId,
         uint256 blockPassId
     );
+    event limitedEditionPassPurchased(
+        address indexed buyer,
+        uint256 tokenId,
+        uint256 blockPassId
+    );
 
     // Constructor initializes the contract
     constructor(
-        address initialOwner
-    ) Ownable(initialOwner) ERC721("Block Pass", "BP") {
+        address initialOwner,
+        address _linkAddress,
+        address _wrapperAddress
+    )
+        Ownable(initialOwner)
+        ERC721("blockpass", "BP")
+        VRFV2WrapperConsumerBase(_linkAddress, _wrapperAddress)
+    {
         tokenId.increment();
     }
 
     /**
-     * @dev Allows a user to purchase a block pass.
-     * @param _blockPassId The ID of the block pass to be purchased.
+     * @dev Allows a user to purchase a blockpass.
+     * @param _blockPassId The ID of the blockpass to be purchased.
      */
     function purchasePass(uint256 _blockPassId) public payable {
         BlockPassDetails storage _pass = getPassById[_blockPassId];
@@ -62,7 +95,7 @@ contract BlockPass is Ownable, ERC721URIStorage {
         require(block.timestamp <= _pass.salesEndTime, "Sales ended");
         require(_pass.passesSold < _pass.max_passes, "Sold out");
 
-        // Mint a new NFT representing the purchased block pass
+        // Mint a new NFT representing the purchased blockpass
         string memory tokenURI = string(abi.encodePacked(_pass.metadata));
         _safeMint(msg.sender, tokenId.current());
         _setTokenURI(tokenId.current(), tokenURI);
@@ -75,24 +108,75 @@ contract BlockPass is Ownable, ERC721URIStorage {
         }("");
         require(success);
 
-        // Update block pass details and user records
+        // Update blockpass details and user records
         _pass.passesSold++;
         bookedPassByUser[msg.sender].push(_pass);
         userNftTokens[msg.sender].push(tokenId.current());
 
-        // Emit event for the booked block pass
+        // Emit event for the booked blockpass
         emit passBooked(msg.sender, tokenId.current(), _pass.blockPassId);
         tokenId.increment();
     }
 
     /**
-     * @dev Allows an organizer to create a new block pass.
-     * @param _max_pass_count The maximum number of passes available for the new block pass.
-     * @param _startTime The start time of the block pass.
-     * @param _salesEndTime The end time of sales for the block pass.
-     * @param _passPrice The price for each block pass.
-     * @param _metadata Additional metadata for the block pass.
-     * @param _category The category of the block pass.
+     * @dev Allows a user to purchase a Limited Edition blockpass ticket.
+     * @param _blockPassId The ID of the blockpass to be purchased.
+     */
+    function purchaseLimitedEditionPass(uint256 _blockPassId) public payable {
+        uint256 requestId = requestRandomEventId();
+        BlockPassDetails storage _pass = getPassById[_blockPassId];
+
+        require(msg.value >= _pass.passPrice);
+        require(block.timestamp <= _pass.salesEndTime, "Sales ended");
+        require(_pass.passesSold < _pass.max_passes, "Sold out");
+        require(
+            requestStatuses[requestId].fulfilled,
+            "Random number not generated yet"
+        );
+
+        // Use the random number to determine if the user gets a limited edition pass
+        uint256 randomNumber = requestStatuses[requestId].randomWords[0];
+        bool isLimitedEdition = (randomNumber % 2 == 0);
+
+        // Mint a new NFT representing the purchased blockpass ticket
+        string memory tokenURI = string(abi.encodePacked(_pass.metadata));
+        _safeMint(msg.sender, tokenId.current());
+        _setTokenURI(tokenId.current(), tokenURI);
+
+        // Calculate fees and transfer funds to the organizer
+        uint256 fee = (msg.value * 10) / 1000;
+        uint256 amountToOrganizer = msg.value - fee;
+        (bool success, ) = payable(_pass.organizer).call{
+            value: amountToOrganizer
+        }("");
+        require(success);
+
+        // Update blockpass details and user records
+        _pass.passesSold++;
+        bookedPassByUser[msg.sender].push(_pass);
+        userNftTokens[msg.sender].push(tokenId.current());
+
+        emit passBooked(msg.sender, tokenId.current(), _pass.blockPassId);
+
+        if (isLimitedEdition) {
+            emit limitedEditionPassPurchased(
+                msg.sender,
+                tokenId.current(),
+                _pass.blockPassId
+            );
+        }
+
+        tokenId.increment();
+    }
+
+    /**
+     * @dev Allows an organizer to create a new blockpass.
+     * @param _max_pass_count The maximum number of passes available for the new blockpass ticket.
+     * @param _startTime The start time of the blockpass.
+     * @param _salesEndTime The end time of sales for the blockpass.
+     * @param _passPrice The price for each blockpass.
+     * @param _metadata Additional metadata for the blockpass.
+     * @param _category The category of the blockpass.
      */
     function createNewPass(
         uint256 _max_pass_count,
@@ -102,7 +186,7 @@ contract BlockPass is Ownable, ERC721URIStorage {
         string memory _metadata,
         string memory _category
     ) external {
-        // Create a new block pass with the provided details
+        // Create a new blockpass with the provided details
         BlockPassDetails memory _pass = BlockPassDetails({
             organizer: msg.sender,
             blockPassId: blockPass_count,
@@ -116,12 +200,12 @@ contract BlockPass is Ownable, ERC721URIStorage {
             bpEnded: false
         });
 
-        // Update mappings and arrays with the new block pass
+        // Update mappings and arrays with the new blockpass
         getPassById[blockPass_count] = _pass;
         blockPassList.push(_pass);
         blockPassCreatedByOrganizer[msg.sender].push(_pass);
 
-        // Increment block pass count and emit event for the creation of a new block pass
+        // Increment blockpass count and emit event for the creation of a new blockpass
         blockPass_count++;
         emit blockPassCreated(
             _pass.organizer,
@@ -131,27 +215,66 @@ contract BlockPass is Ownable, ERC721URIStorage {
     }
 
     /**
-     * @dev Allows an organizer to update the timing of a block pass.
-     * @param _blockPassId The ID of the block pass to be updated.
-     * @param _newStartTime The new start time for the block pass.
-     * @param _newSalesEndTime The new end time for sales of the block pass.
+     * @dev Allows an organizer to update the timing of a blockpass.
+     * @param _blockPassId The ID of the blockpass to be updated.
+     * @param _newStartTime The new start time for the blockpass.
+     * @param _newSalesEndTime The new end time for sales of the blockpass.
      */
     function updatePassTiming(
         uint256 _blockPassId,
         uint256 _newStartTime,
         uint256 _newSalesEndTime
     ) external {
-        // Update the timing of the specified block pass
+        // Update the timing of the specified blockpass
         BlockPassDetails storage _pass = getPassById[_blockPassId];
         _pass.startTime = block.timestamp + _newStartTime;
         _pass.salesEndTime = block.timestamp + _newSalesEndTime;
+    }
+
+    function requestRandomEventId() public returns (uint256 requestId) {
+        requestId = requestRandomness(
+            callbackGasLimit,
+            requestConfirmations,
+            numWords
+        );
+        requestStatuses[requestId] = RequestStatus(
+            VRF_V2_WRAPPER.calculateRequestPrice(callbackGasLimit),
+            false,
+            new uint256[](0)
+        );
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(requestStatuses[_requestId].paid > 0, "request not found");
+        requestStatuses[_requestId].fulfilled = true;
+        requestStatuses[_requestId].randomWords = _randomWords;
+        uint256 passId = uint256(keccak256(abi.encodePacked(_randomWords)));
+        BlockPassDetails memory _pass = getPassById[passId];
+        _pass.blockPassId = _randomWords[0] % 1000;
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    )
+        external
+        view
+        returns (uint256 paid, bool fulfilled, uint256[] memory randomWords)
+    {
+        require(requestStatuses[_requestId].paid > 0, "request not found");
+        RequestStatus memory request = requestStatuses[_requestId];
+        return (request.paid, request.fulfilled, request.randomWords);
     }
 
     /**
      * @dev Allows the contract owner to withdraw funds from the contract.
      */
     function withdraw() public onlyOwner {
-        // Transfer contract balance to the owner
         (bool success, ) = payable(owner()).call{value: address(this).balance}(
             ""
         );
@@ -172,9 +295,9 @@ contract BlockPass is Ownable, ERC721URIStorage {
     }
 
     /**
-     * @dev Retrieves the block passes booked by a specific user.
+     * @dev Retrieves the blockpasses booked by a specific user.
      * @param _user The address of the user.
-     * @return An array of block passes booked by the user.
+     * @return An array of blockpasses booked by the user.
      */
     function blockPassesBookedByUser(
         address _user
@@ -183,9 +306,9 @@ contract BlockPass is Ownable, ERC721URIStorage {
     }
 
     /**
-     * @dev Retrieves the block passes created by a specific organizer.
+     * @dev Retrieves the blockpasses created by a specific organizer.
      * @param _organizer The address of the organizer.
-     * @return An array of block passes created by the organizer.
+     * @return An array of blockpasses created by the organizer.
      */
     function organizerBlockPasses(
         address _organizer
@@ -194,16 +317,16 @@ contract BlockPass is Ownable, ERC721URIStorage {
     }
 
     /**
-     * @dev Retrieves the total number of block passes in the contract.
-     * @return The total number of block passes.
+     * @dev Retrieves the total number of blockpasses in the contract.
+     * @return The total number of blockpasses.
      */
-    function totalNumberOfBlockPassList() public view returns (uint) {
+    function totalNumberOfBlockPassList() public view returns (uint256) {
         return blockPassList.length;
     }
 
     /**
-     * @dev Retrieves an array of all block passes in the contract.
-     * @return An array of block passes.
+     * @dev Retrieves an array of all blockpasses in the contract.
+     * @return An array of blockpasses.
      */
     function allBlockPassList()
         public
@@ -214,40 +337,40 @@ contract BlockPass is Ownable, ERC721URIStorage {
     }
 
     /**
-     * @dev Retrieves the number of passes sold for a specific block pass.
-     * @param _blockPassId The ID of the block pass.
-     * @return The number of passes sold for the block pass.
+     * @dev Retrieves the number of passes sold for a specific blockpass.
+     * @param _blockPassId The ID of the blockpass.
+     * @return The number of passes sold for the blockpass.
      */
     function passesSoldForBlockPass(
         uint256 _blockPassId
-    ) public view returns (uint) {
+    ) public view returns (uint256) {
         return getPassById[_blockPassId].passesSold;
     }
 
     /**
-     * @dev Checks if a specific block pass has ended.
-     * @param _blockPassId The ID of the block pass.
-     * @return A boolean indicating whether the block pass has ended.
+     * @dev Checks if a specific blockpass has ended.
+     * @param _blockPassId The ID of the blockpass.
+     * @return A boolean indicating whether the blockpass has ended.
      */
-    function isBlockPassOver(uint _blockPassId) public view returns (bool) {
+    function isBlockPassOver(uint256 _blockPassId) public view returns (bool) {
         return getPassById[_blockPassId].bpEnded;
     }
 
     /**
-     * @dev Retrieves an array of block passes belonging to a specific category.
-     * @param _category The category of block passes to retrieve.
-     * @return An array of block passes in the specified category.
+     * @dev Retrieves an array of blockpasses belonging to a specific category.
+     * @param _category The category of blockpasses to retrieve.
+     * @return An array of blockpasses in the specified category.
      */
     function getByCategory(
         string memory _category
     ) public view returns (BlockPassDetails[] memory) {
-        uint i = 0;
-        uint arrayCount = 0;
+        uint256 i = 0;
+        uint256 arrayCount = 0;
         BlockPassDetails[] memory blockPassCategory = new BlockPassDetails[](
             blockPassList.length
         );
 
-        // Iterate through all block passes and filter by category
+        // Iterate through all blockpasses and filter by category
         for (; i < blockPassList.length; i++) {
             BlockPassDetails memory currentBlockPass = blockPassList[i];
 
@@ -264,12 +387,12 @@ contract BlockPass is Ownable, ERC721URIStorage {
     }
 
     /**
-     * @dev Allows an organizer to remove a block pass they created.
-     * @param _blockPassId The ID of the block pass to be removed.
+     * @dev Allows an organizer to remove a blockpass they created.
+     * @param _blockPassId The ID of the blockpass to be removed.
      */
-    function removeBlockPass(uint _blockPassId) external {
-        uint i = 0;
-        uint envIndex = 0;
+    function removeBlockPass(uint256 _blockPassId) external {
+        uint256 i = 0;
+        uint256 envIndex = 0;
         BlockPassDetails storage _pass = getPassById[_blockPassId];
         require(_pass.organizer == msg.sender, "Not Authorized");
         require(_pass.passesSold == 0, "Passes already bought");
@@ -284,7 +407,7 @@ contract BlockPass is Ownable, ERC721URIStorage {
             }
         }
         for (
-            uint index = i;
+            uint256 index = i;
             index < blockPassCreatedByOrganizer[msg.sender].length - 1;
             index++
         ) {
@@ -302,7 +425,11 @@ contract BlockPass is Ownable, ERC721URIStorage {
             }
         }
 
-        for (uint index = envIndex; index < blockPassList.length - 1; index++) {
+        for (
+            uint256 index = envIndex;
+            index < blockPassList.length - 1;
+            index++
+        ) {
             blockPassList[index] = blockPassList[index + 1];
         }
         blockPassList.pop();
@@ -312,11 +439,11 @@ contract BlockPass is Ownable, ERC721URIStorage {
     }
 
     /**
-     * @dev Allows the contract owner to remove a block pass.
-     * @param _blockPassId The ID of the block pass to be removed.
+     * @dev Allows the contract owner to remove a blockpass.
+     * @param _blockPassId The ID of the blockpass to be removed.
      */
     function removeBlockPassAdmin(uint256 _blockPassId) external onlyOwner {
-        uint envIndex = 0;
+        uint256 envIndex = 0;
         // Remove from general blockPassList array
 
         for (; envIndex < blockPassList.length - 1; envIndex++) {
@@ -326,7 +453,11 @@ contract BlockPass is Ownable, ERC721URIStorage {
             }
         }
 
-        for (uint index = envIndex; index < blockPassList.length - 1; index++) {
+        for (
+            uint256 index = envIndex;
+            index < blockPassList.length - 1;
+            index++
+        ) {
             blockPassList[index] = blockPassList[index + 1];
         }
         blockPassList.pop();
