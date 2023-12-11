1dcff0d7d179c2913984a334f35b3ef56498fd4b
dulaz41 <dulaz41@yahoo.com>
2023-12-11 12:02:41 +0100

    Added abi and Ca

--- Frontend/components/Dashboard/Events.tsx
+++ Frontend/components/Dashboard/Events.tsx
@@ -1,11 +1,11 @@
-import { Box, Center, Icon, Text } from '@chakra-ui/react';
-import { ethers } from 'ethers';
-import React, { useCallback, useEffect, useState } from 'react';
-import { FaExclamation } from 'react-icons/fa';
-import { NETWORKS } from '../../config/networks';
-import { useWeb3 } from '../../context/Web3Context';
-import LoadingSpinner from '../Alerts/LoadingSpinner';
-import NoWalletAlert from '../Alerts/NoWalletAlert';
+import { Box, Center, Icon, Text } from "@chakra-ui/react";
+import { ethers } from "ethers";
+import React, { useCallback, useEffect, useState } from "react";
+import { FaExclamation } from "react-icons/fa";
+import { NETWORKS } from "../../config/networks";
+import { useWeb3 } from "../../context/Web3Context";
+import LoadingSpinner from "../Alerts/LoadingSpinner";
+import NoWalletAlert from "../Alerts/NoWalletAlert";
 // Replace with your Alchemy API key:
 const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_KEY;
 const baseURL = `https://polygon-mumbai.g.alchemy.com/v2/${apiKey}`;
@@ -19,16 +19,16 @@ function MyEvents() {
   });
 
   const fetchEvents = useCallback(async () => {
-    let Mrpc: string = 'https://polygon-mumbai.g.alchemy.com/v2/' + apiKey;
-    let Frpc: string = 'https://api.hyperspace.node.glif.io/rpc/v1';
+    let Mrpc: string = "https://polygon-mumbai.g.alchemy.com/v2/" + apiKey;
+    let Frpc: string = "https://api.avax-test.network/ext/C/rpc";
 
     const Mprovider = new ethers.providers.JsonRpcProvider(Mrpc);
     let Mca = NETWORKS.polygon_mumbai.ca;
     let Mabi = NETWORKS.polygon_mumbai.abi;
 
     const Fprovider = new ethers.providers.JsonRpcProvider(Frpc);
-    let Fca = NETWORKS.fil_testnet.ca;
-    let Fabi = NETWORKS.fil_testnet.abi;
+    let Fca = NETWORKS.fuji_testnet.ca;
+    let Fabi = NETWORKS.fuji_testnet.abi;
 
     const Mct = new ethers.Contract(Mca, Mabi, Mprovider);
     const Fct = new ethers.Contract(Fca, Fabi, Fprovider);
@@ -58,9 +58,9 @@ function MyEvents() {
 
       {/* {client && !events.loading && events.events.length > 0 && <TicketsGrid nft={events.events} />} */}
       {!events.loading && events.events && events.events.length === 0 && (
-        <Center flexDir='column' p='5' gap='3'>
-          <Icon as={FaExclamation} fontSize='30px' />
-          <Text fontSize='lg'>You haven&apos;t created any events yet!</Text>
+        <Center flexDir="column" p="5" gap="3">
+          <Icon as={FaExclamation} fontSize="30px" />
+          <Text fontSize="lg">You haven&apos;t created any events yet!</Text>
         </Center>
       )}
       {events.loading && <LoadingSpinner />}
